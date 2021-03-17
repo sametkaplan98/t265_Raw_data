b9c49355c7f85228f9a9d4b05ce37dfe7a2be12f
Samet <47507568+sametkaplan98@users.noreply.github.com>
2021-03-17 23:04:32 +0300

    Update stabil.py

--- stabil.py
+++ stabil.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Created on Wed Mar 17 22:59:02 2021
+
+@author: samet
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
 Created on Sun Mar 14 23:54:27 2021
 
 @author: samet
@@ -9,7 +17,6 @@ Created on Sun Mar 14 23:54:27 2021
 import pyrealsense2 as rs
 import numpy as np
 import cv2
-from math import tan, pi
 
 
 #------------------------------------------------------------------------------
@@ -45,22 +52,25 @@ config.enable_stream(rs.stream.fisheye,1)
 config.enable_stream(rs.stream.fisheye,2)
 config.enable_stream(rs.stream.accel)
 config.enable_stream(rs.stream.gyro)
+#4 tane stream enable edildiği için frame üzerinden 0-1-2-3 indexleri ile stream'lere ulaşılabilir
 profile = pipeline.start(config)
 
 try:
     while True:
         frames = pipeline.wait_for_frames()
-        nir_lf_frame = frames.get_fisheye_frame(1)
-        nir_rg_frame = frames.get_fisheye_frame(2)
+        left_frame = frames.get_fisheye_frame(1) 
+        right_frame = frames.get_fisheye_frame(2)
         accel = accel_data(frames[2].as_motion_frame().get_motion_data())
         gyro = gyro_data(frames[3].as_motion_frame().get_motion_data())
-        nir_lf_image = np.asanyarray(nir_lf_frame.get_data())
-        nir_rg_image = np.asanyarray(nir_rg_frame.get_data())
-     
+        left_image = np.asanyarray(left_frame.get_data())
+        right_image = np.asanyarray(right_frame.get_data())
+
         print(gyro, accel)
         
         # image=np.hstack((nir_lf_image,nir_rg_image)) -> iki goruntu birlesik
-        cv2.imshow('Fisheye goruntu', nir_rg_image)       
+        cv2.imshow('Fisheye goruntu', right_image)
+        
+
         
         key = cv2.waitKey(1)
         if key & 0xFF == ord('q') or key == 27:
@@ -68,4 +78,3 @@ try:
             break
 finally:
         pipeline.stop()
-
