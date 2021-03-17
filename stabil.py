64d5cf6973360cac3766146a917af1fb341e8c87
Samet <47507568+sametkaplan98@users.noreply.github.com>
2021-03-17 22:52:29 +0300

    Update stabil.py

--- stabil.py
+++ stabil.py
@@ -58,97 +58,11 @@ try:
         gyro = gyro_data(frames[3].as_motion_frame().get_motion_data())
         nir_lf_image = np.asanyarray(nir_lf_frame.get_data())
         nir_rg_image = np.asanyarray(nir_rg_frame.get_data())
-        '''
-        frame_data["left"] =  nir_lf_image
-        frame_data["right"] = nir_rg_image
-        '''
+     
         print(gyro, accel)
         
         # image=np.hstack((nir_lf_image,nir_rg_image)) -> iki goruntu birlesik
-        cv2.imshow('Fisheye goruntu', nir_rg_image)
-        
-        
-        '''
-        window_size = 5
-        min_disp = 0
-        # must be divisible by 16
-        num_disp = 112 - min_disp
-        max_disp = min_disp + num_disp
-        stereo = cv2.StereoSGBM_create(minDisparity = min_disp,
-                                   numDisparities = num_disp,
-                                   blockSize = 16,
-                                   P1 = 8*3*window_size**2,
-                                   P2 = 32*3*window_size**2,
-                                   disp12MaxDiff = 1,
-                                   uniquenessRatio = 10,
-                                   speckleWindowSize = 100,
-                                   speckleRange = 32)
-        profiles = pipeline.get_active_profile()
-
-        # Retreive the stream and intrinsic properties for both cameras
-        streams = {"left"  : profiles.get_stream(rs.stream.fisheye, 1).as_video_stream_profile(),
-               "right" : profiles.get_stream(rs.stream.fisheye, 2).as_video_stream_profile()}
-        intrinsics = {"left"  : streams["left"].get_intrinsics(),
-                  "right" : streams["right"].get_intrinsics()}
-        
-        K_left  = camera_matrix(intrinsics["left"])
-        D_left  = fisheye_distortion(intrinsics["left"])
-        K_right = camera_matrix(intrinsics["right"])
-        D_right = fisheye_distortion(intrinsics["right"])
-        (width, height) = (intrinsics["left"].width, intrinsics["left"].height)
-        
-        (R, T) = get_extrinsics(streams["left"], streams["right"])
-
-        stereo_fov_rad = 90 * (pi/180)  # 90 degree desired fov
-        stereo_height_px = 300          # 300x300 pixel stereo output
-        stereo_focal_px = stereo_height_px/2 / tan(stereo_fov_rad/2)
-
-        R_left = np.eye(3)
-        R_right = R
-
-
-        stereo_width_px = stereo_height_px + max_disp
-        stereo_size = (stereo_width_px, stereo_height_px)
-        stereo_cx = (stereo_height_px - 1)/2 + max_disp
-        stereo_cy = (stereo_height_px - 1)/2
-
-        P_left = np.array([[stereo_focal_px, 0, stereo_cx, 0],
-                       [0, stereo_focal_px, stereo_cy, 0],
-                       [0,               0,         1, 0]])
-        P_right = P_left.copy()
-        P_right[0][3] = T[0]*stereo_focal_px
-
-
-        Q = np.array([[1, 0,       0, -(stereo_cx - max_disp)],
-                  [0, 1,       0, -stereo_cy],
-                  [0, 0,       0, stereo_focal_px],
-                  [0, 0, -1/T[0], 0]])
-
-        m1type = cv2.CV_32FC1
-        (lm1, lm2) = cv2.fisheye.initUndistortRectifyMap(K_left, D_left, R_left, P_left, stereo_size, m1type)
-        (rm1, rm2) = cv2.fisheye.initUndistortRectifyMap(K_right, D_right, R_right, P_right, stereo_size, m1type)
-        undistort_rectify = {"left"  : (lm1, lm2),
-                         "right" : (rm1, rm2)}
-
-        mode = "stack"
-   
-        frame_copy = {"left"  : frame_data["left"].copy(),
-                          "right" : frame_data["right"].copy()}
-        center_undistorted = {"left" : cv2.remap(src = frame_copy["left"],
-                                          map1 = undistort_rectify["left"][0],
-                                          map2 = undistort_rectify["left"][1],
-                                          interpolation = cv2.INTER_LINEAR),
-                                  "right" : cv2.remap(src = frame_copy["right"],
-                                          map1 = undistort_rectify["right"][0],
-                                          map2 = undistort_rectify["right"][1],
-                                          interpolation = cv2.INTER_LINEAR)}
-
-       
-
-        color_image = cv2.cvtColor(center_undistorted["left"][:,max_disp:], cv2.COLOR_GRAY2RGB)
-        
-        cv2.imshow("Birlesik", color_image)
-        '''
+        cv2.imshow('Fisheye goruntu', nir_rg_image)       
         
         key = cv2.waitKey(1)
         # Press esc or 'q' to close the image window
